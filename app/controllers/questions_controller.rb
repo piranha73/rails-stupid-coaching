3efc4dcda82d88735e1f9d900abaa33f22c882b8
Michele Comolli <mikicomolli@gmail.com>
2020-10-19 15:40:01 +0200

    Challenge 2 done!

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,12 @@
+class QuestionsController < ApplicationController
+  def ask; end
+  def answer
+    if params[:question] == 'I am going to work'
+      @message = 'Great'
+    elsif params[:question][-1] == '?'
+      @message = 'Silly question, get dressed and go to work!'
+    else
+      @message = "I don't care, get dressed and go to work!"
+    end
+  end
+end
